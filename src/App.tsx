3a62050b1af9d5d1e1842fb0ad46e1fc07911e90
Artem Rybin <catobazzz@gmail.com>
2024-03-05 14:51:31 +0300

    add btn component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Box>
+            {/*<Text> AAAAAAAAAAAaaaaa ddddd </Text>*/}
+            <div>
+                <Button > See more </Button>
+                <Button > Save </Button>
+            </div>
+
+        </Box>
+
+
     </div>
   );
 }
 
 export default App;
+
+// const Text = styled.text`
+//   color: rgb(171, 179, 186);
+//   font-family: Inter,serif;
+//   font-size: 12px;
+//   font-weight: 500;
+//   line-height: 20px;
+//   //letter-spacing: 0%;
+//   text-align: left;
+// `
+
+const Button = styled.button`
+  border-radius: 5px;
+  width: 86px;
+  height: 30px;
+`
+const Box = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+`
